a2d2331854fbfc2e6ba66aa86c596922c9828360
alwayswannafly <15007159069@163.com>
2020-02-28 20:34:51 +0800

    类型

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
-var fn = function () { return 'hello world'; };
+{
+    var arr = [1, 2, 3];
+    console.log(arr);
+    var myArr = ['q', 'w'];
+    myArr.push('0');
+    console.log(myArr);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+{
+    //tuple 元组，里面的元素可以多个类型，个数是固定的
+    var my_tuple = ['aaa', 99];
+    console.log(my_tuple[0]);
+    // let  aa:[string, number] 
+    // aa =[22,'33']
+    // console.log(aa);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+{
+    var add = function (a, b) {
+        return a + b;
+    };
+    console.log(add(1, 2));
+    //指定函数返回值的类型
+    var add2 = function (a, b) {
+        return a.toString() + b.toString();
+    };
+    console.log(add2(10, 20).substr(0, 3));
+    //默认值
+    var add3 = function (a, b) {
+        if (b === void 0) { b = 20; }
+        console.log(a + b);
+    };
+    add3(10);
+    add3(10, 40);
+    var add4 = function (a, b) {
+        if (!b) {
+            console.log(a);
+        }
+        else {
+            console.log(a + b);
+        }
+    };
+    add4(10, 20);
+    add4(10);
+    var add5 = function (a) {
+        var num = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            num[_i - 1] = arguments[_i];
+        }
+        return num.reduce(function (total, num) {
+            return total + num;
+        }, a);
+    };
+    var sum = add5(10, 20, 30, 40, 50);
+    console.log(sum);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+{
+    //任意类型
+    var a = void 0;
+    a = 10;
+    a = 'sss';
+    // const log = (value: any) => {
+    //     if (typeof value === 'number') {
+    //         return 1
+    //     }
+    //     if (typeof value === 'string') {
+    //         return 2
+    //     }
+    //     throw new Error(`ss`)
+    // }
+    // console.log(log(1));
+    // let b: any[]
+    // b = [1, 2]
+    // b = ['2', 2, [4, '55']]
+    var isNumber_1 = function (value) {
+        return typeof value === 'number';
+    };
+    var isString_1 = function (value) {
+        return typeof value === 'string';
+    };
+    var log = function (value) {
+        if (isNumber_1(value)) {
+            return 1;
+        }
+        if (isString_1(value)) {
+            return 2;
+        }
+        throw new Error("ss");
+    };
+    console.log(log(true));
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,102 @@
-var fn = function () { return 'hello world'; };
+{
+    let arr: number[] = [1, 2, 3]
+    console.log(arr);
+
+    let myArr: Array<string> = ['q', 'w']
+    myArr.push('0')
+    console.log(myArr);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+
+{
+    //tuple 元组，里面的元素可以多个类型，个数是固定的
+    let my_tuple: [string, number] = ['aaa', 99]
+    console.log(my_tuple[0]);
+
+    // let  aa:[string, number] 
+    // aa =[22,'33']
+    // console.log(aa);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+
+{
+    const add = (a: number, b: number) => {
+        return a + b
+    }
+    console.log(add(1, 2));
+
+    //指定函数返回值的类型
+    const add2 = (a: number, b: number): string => {
+        return a.toString() + b.toString()
+    }
+    console.log(add2(10, 20).substr(0, 3));
+
+    //默认值
+    const add3 = (a: number, b = 20): void => {
+        console.log(a + b);
+    }
+    add3(10)
+    add3(10, 40)
+
+    const add4 = (a: number, b?: number): void => {
+        if (!b) {
+            console.log(a);
+
+        } else {
+            console.log(a + b);
+        }
+    }
+
+    add4(10, 20)
+    add4(10)
+
+    const add5 = (a: number, ...num: number[]): number => {
+        return num.reduce((total: number, num): number => {
+            return total + num
+        }, a)
+    }
+    let sum = add5(10, 20, 30, 40, 50)
+    console.log(sum);
+    console.log('~~~~~~~~~~____~~~~~~~~~~~');
+}
+
+{
+    //任意类型
+    let a: any
+    a = 10;
+    a = 'sss'
+
+    // const log = (value: any) => {
+    //     if (typeof value === 'number') {
+    //         return 1
+    //     }
+    //     if (typeof value === 'string') {
+    //         return 2
+    //     }
+    //     throw new Error(`ss`)
+    // }
+
+    // console.log(log(1));
+
+    // let b: any[]
+    // b = [1, 2]
+    // b = ['2', 2, [4, '55']]
+
+    const isNumber = (value: any): value is number => {
+        return typeof value === 'number'
+    }
+    const isString = (value: any): value is string => {
+        return typeof value === 'string'
+    }
+    const log = (value: any) => {
+        if (isNumber(value)) {
+            return 1
+        }
+        if (isString(value)) {
+            return 2
+        }
+        throw new Error(`ss`)
+    }
+    console.log(log(true));
+    
+}
